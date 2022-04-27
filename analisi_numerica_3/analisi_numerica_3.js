8e89ebf0e8893416c5f5efa0b4c197e97f605355
Driu04 <95434452+Driu04@users.noreply.github.com>
2022-04-27 10:16:45 +0200

    Update analisi_numerica_3.js

--- analisi_numerica_3/analisi_numerica_3.js
+++ analisi_numerica_3/analisi_numerica_3.js
@@ -4,9 +4,21 @@ function analisiNum() {
     var range = parseInt(document.getElementById("s1").value);
     let divisibile = 0;
     let num_primi = "";
+    let divis = 0;
 
-    if (range > 1)
-        num_primi = "Numeri primi tra 1 e " + range + "" + ":<br>";
+    if (range > 1) {
+         for (let i = 1; i < range; i ++) {
+            if (range % i == 0)
+                divis++;
+        }
+        
+        if (divis == 1)
+            num_primi = range + " e' un numero primo<br>";
+        else
+            num_primi = range + " non e' un numero primo<br>";
+
+        num_primi += "Numeri primi tra 1 e " + range + "" + ":<br>";
+    }
 
     for(let i=1; i<range; i++) {
         for(let j=1; j<i + 1; j++) {
